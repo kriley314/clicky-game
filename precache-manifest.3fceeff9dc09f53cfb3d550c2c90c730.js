9cc508a08e24a6ed8ded90d898e456b9dbc5e82b
Ken Riley <kriley314@gmail.com>
2019-05-01 16:27:35 -0500

    Updates

---
+++ precache-manifest.3fceeff9dc09f53cfb3d550c2c90c730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4086298bd28954ca7867695136dd0956",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "673d8cc6c9628b390260",
+    "url": "/clicky-game/static/css/main.bbf9b676.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "673d8cc6c9628b390260",
+    "url": "/clicky-game/static/js/main.33e684b3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
